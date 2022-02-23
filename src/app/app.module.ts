2ea0d53021f3e5167bddc2d4fba6ed1d6980ff5a
Gabriela Escobedo <gescobedo@epconccs.com>
2022-02-23 09:29:05 -0600

    Empleados-estructura-inical

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmpleadoComponent } from './components/Empleados/empleado/empleado.component';
+import { FormularioEmpleadosComponent } from './components/Empleados/empleado/formularioEmpleados/formulario-empleados/formulario-empleados.component';
+import { ListaEmpleadosComponent } from './components/Empleados/empleado/listaEmpleados/lista-empleados/lista-empleados.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmpleadoComponent,
+    FormularioEmpleadosComponent,
+    ListaEmpleadosComponent
   ],
   imports: [
     BrowserModule,
